e79970511a45e8baeb2c71cf019af6232f2f209e
NavinRem <sonavin.rem@gmail.com>
2026-03-03 12:09:42 +0700

    Add clickable enrollment table rows and beautifully styled Registration Details view matching the design

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -22,6 +22,12 @@ const router = createRouter({
       meta: { title: 'Enrollment' },
     },
     {
+      path: '/enrollment/:id',
+      name: 'enrollment-detail',
+      component: () => import('../views/EnrollmentDetail.vue'),
+      meta: { title: 'Registration Detail' },
+    },
+    {
       path: '/parents',
       name: 'parents',
       component: () => import('../views/Parents.vue'),
